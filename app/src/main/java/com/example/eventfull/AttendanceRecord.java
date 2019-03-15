8acb114c5343a86f38af390c7b87cd0ac5ec901c
eugene2470 <eugene2470@gmail.com>
2019-03-15 16:32:36 +0000

    .

---
+++ app/src/main/java/com/example/eventfull/AttendanceRecord.java
@@ -0,0 +1,12 @@
+package com.example.eventfull;
+
+public class AttendanceRecord {
+
+    private int unattendedCount;
+
+    public static void incrementCount() {}
+
+    public static void resetCount() {}
+
+    public int getCount(){return unattendedCount;}
+}
