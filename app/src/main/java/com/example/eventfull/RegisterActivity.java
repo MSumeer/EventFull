34f23010e483c71d97afdd28d60bb3683c1e0c7f
OrlandoRati <orlando.rati@outlook.com>
2019-03-21 22:09:19 +0000

    Saving registered user to json

--- app/src/main/java/com/example/eventfull/RegisterActivity.java
+++ app/src/main/java/com/example/eventfull/RegisterActivity.java
@@ -2,11 +2,97 @@ package com.example.eventfull;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registeractivity);
+
+        //Assigning text from the text boxes to variables
+        final EditText etFirstName = (EditText) findViewById(R.id.etFirstName);
+        final EditText etLastName = (EditText) findViewById(R.id.etLastName);
+        final EditText etEmail = (EditText) findViewById(R.id.etEmail);
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+
+        //Assigning the register button to a variable
+        final Button btnRegister = (Button) findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Assign text from text boxes to variables
+                String firstName = etFirstName.getText().toString().trim();
+                String lastName = etLastName.getText().toString().trim();
+                String email = etEmail.getText().toString().trim();
+                String username = etUsername.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+
+                //Create a JSONObject to store values
+                try {
+                    JSONObject obj = new JSONObject();
+                    obj.put("firstName", firstName);
+                    obj.put("lastName", lastName);
+                    obj.put("email", email);
+                    obj.put("username", username);
+                    obj.put("password", password);
+
+                    //Write JSONObject to file
+
+                    FileWriter file = new FileWriter("users.json");
+                    file.write(obj.toString());
+                    file.flush();
+
+                    AlertDialog alertDialog = new AlertDialog.Builder(RegisterActivity.this).create();
+                    alertDialog.setTitle("Alert");
+                    alertDialog.setMessage(firstName+lastName+email+username+password);
+                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alertDialog.show();
+                }
+                catch(JSONException e){
+                    AlertDialog alertDialog = new AlertDialog.Builder(RegisterActivity.this).create();
+                    alertDialog.setTitle("Alert");
+                    alertDialog.setMessage("ErrorJSON");
+                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alertDialog.show();
+                }
+                catch(IOException e){
+                    AlertDialog alertDialog = new AlertDialog.Builder(RegisterActivity.this).create();
+                    alertDialog.setTitle("Alert");
+                    alertDialog.setMessage("ErrorIO");
+                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alertDialog.show();
+                }
+            }
+        });
+
     }
 }
