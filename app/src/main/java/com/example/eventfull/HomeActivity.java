49a985515b3f9e235d187a8fea648547dc46bb4f
eugene2470 <eugene2470@gmail.com>
2019-03-25 01:33:32 +0000

    update display list

--- app/src/main/java/com/example/eventfull/HomeActivity.java
+++ app/src/main/java/com/example/eventfull/HomeActivity.java
@@ -4,19 +4,28 @@ import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.SearchView;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class HomeActivity extends AppCompatActivity {
+    String[] data;
+    SearchView searchView;
+    ListView listView;
     User user = null;
     //onCreate() method invoked once the application starts
     @Override
@@ -24,14 +33,30 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homeactivity);
         String userName = load();
-        user = Registry.getInstance().getUser(userName,getApplicationContext());
+        user = Registry.getInstance().getUser(userName, getApplicationContext());
         Intent intent = getIntent();
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
             //doMySearch(query);
         }
 
+        JSONArray read = Registry.getInstance().read(getApplicationContext(), "Events.txt");
+        ArrayList<String> items = new ArrayList<String>();
+        try {
+
+            for (int i = 0; i < read.length(); i++) {
+                JSONObject line = read.getJSONObject(i);
+                String match = line.optString("price");
+                items.add(match);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        ListView mylistview = findViewById(R.id.eventList);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        mylistview.setAdapter(arrayAdapter);
     }
+
     public String load(){
         FileInputStream fis;
         String userName="";
@@ -54,11 +79,17 @@ public class HomeActivity extends AppCompatActivity {
 
     public void searchmethod() {
 
-        JSONArray search= Registry.getInstance().read(getApplicationContext(), "Events.txt");
-        //EditText what = findViewById(R.id.what);
-        //EditText where = findViewById(R.id.where);
-        //EditText When = findViewById(R.id.when);
+
+
+        EditText what = findViewById(R.id.what);
+        EditText where = findViewById(R.id.where);
+        EditText When = findViewById(R.id.when);
 
 
     }
+
+
+
+
+
 }
