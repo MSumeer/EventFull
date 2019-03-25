c9cc868720954c43b9c7725938f8005614d001c4
bushra <bushrasmoh@gmail.com>
2019-03-25 05:45:10 +0000

    addition of 'RELIST TICKETS' page

---
+++ app/src/main/java/com/example/eventfull/RelistTicket.java
@@ -0,0 +1,13 @@
+package com.example.eventfull;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RelistTicket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relist_ticket);
+    }
+}
